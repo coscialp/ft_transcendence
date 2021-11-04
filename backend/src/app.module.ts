56f1f50a45e7f90565c92e55a3e602d56251561a
coscialp <corentin.scialpi@gmail.com>
2021-11-04 12:31:59 +0100

    user init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuthModule],
+  imports: [TypeOrmModule.forRoot(), AuthModule, UserModule],
 })
 export class AppModule {}
