3f828e0fb2612a89301836aad144ff5a2cb5dee7
akerdeka <kerdea@yahoo.fr>
2021-11-18 13:50:20 +0100

    Merge master

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,10 +1,14 @@
+import axios from 'axios';
 import React from 'react'
+import { useCookies } from 'react-cookie';
 import { useHistory } from 'react-router'
 import './navbar.css'
 
 export function NavBar(props: any) {
   
   let history = useHistory();
+  var search: string | number | readonly string[] | undefined;
+  const [cookies] = useCookies();
 
   function handleClickPlay() {
     return (
@@ -18,13 +22,38 @@ export function NavBar(props: any) {
     )
   }
 
+  function handleInputSearch(e: any) {
+    search = e.target.value;
+  }
+
+  function handleSearch() {
+    console.log(search)
+  }
+
+  function handleProfile() {
+    console.log("Profile menu !")
+  }
+
+  function loadProfilePicture() {
+  
+  axios.request({
+      url: '/user/me/avatar',
+      method: 'get',
+      baseURL: 'http://localhost:5000',
+      headers: {
+        "Authorization": `Bearer ${cookies.access_token}`,
+      }
+    }).then((response: any) => { response =  response.data; })
+    return '/Beluga.jpeg';
+  }
+
   return(
     <div className="navBar">
       <button className="navBtn" onClick={ handleClickHome } ><h1 className={ props.page==="Home" ? "neonTextOn" : "neonTextOff"}>Home</h1></button>
       <button className="navBtn" onClick={ handleClickPlay } ><h1 className={ props.page==="Play" ? "neonTextOn" : "neonTextOff"}>Play</h1></button>
         <div className="prof-search">
-          <input type="text" className="searchBar" placeholder="Search" />
-          <img src="/Beluga.jpeg" alt="" className="navProfile" onClick={() => console.log("Clicked !")}></img>
+          <input type="text" className="searchBar" placeholder="Search" value={ search } onChange={ handleInputSearch } onSubmit={ handleSearch } />
+          <img src={ loadProfilePicture() } alt="" className="navProfile" onClick={ handleProfile }></img>
         </div>
     </div>
   )
