17867a50d14c7f843e6b50dcc441cfd37ab07985
akerdeka <kerdea@yahoo.fr>
2021-12-16 16:11:41 +0100

    changes and bugs corrections

--- frontend/src/components/home/friendlist.tsx
+++ frontend/src/components/home/friendlist.tsx
@@ -1,31 +1,33 @@
 import axios from "axios"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 
 const ip = window.location.hostname;
 
 export function Friendlist() {
-    const [cookies] = useCookies();
-    const [friends, setFriends]: any = useState([]);
+  const [cookies] = useCookies();
+  const [friends, setFriends]: any = useState([]);
 
 
+  useEffect(() => {
     axios.request({
-        url: '/user/me/friends',
-        method: 'get',
-        baseURL: `http://${ip}:5000`,
-        headers: {
-          "Authorization": `Bearer ${cookies.access_token}`,
-        }
-        }).then((response: any) => {
-          setFriends(response.data.friends)
-        })
+      url: '/user/me/friends',
+      method: 'get',
+      baseURL: `http://${ip}:5000`,
+      headers: {
+        "Authorization": `Bearer ${cookies.access_token}`,
+      }
+    }).then((response: any) => {
+      setFriends(response.data.friends)
+    })
+  }, [cookies])
 
-    return (
-        <div className="FriendElement" >
-            <p className="FriendTitle" >Friend List</p>
-            {friends.map((friends: any) => (
-              <div className="FriendList" key={friends.id} >{friends.username}</div>
-            ))}
-        </div>
-    )
+  return (
+    <div className="FriendElement" >
+      <p className="FriendTitle" >Friend List</p>
+      {friends.map((friends: any) => (
+        <div className="FriendList" key={friends.id} >{friends.username}</div>
+      ))}
+    </div>
+  )
 }
