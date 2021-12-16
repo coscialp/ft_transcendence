25e56c7b3f50500adc13689a51076c9441fd9e3a
acharras <charras2311@gmail.com>
2021-12-16 15:27:14 +0100

    friendlist

--- frontend/src/components/home/friendlist.tsx
+++ frontend/src/components/home/friendlist.tsx
@@ -1,10 +1,26 @@
 import axios from "axios"
+import { useEffect, useState } from 'react';
 import { useCookies } from "react-cookie";
 
 const ip = window.location.hostname;
 
 export function Friendlist() {
     const [cookies] = useCookies();
+    const ip = window.location.hostname;
+    const [friend, setFriend]: any = useState([]);
+
+    useEffect(() => {
+      axios.request({
+          url: `/home`,
+          method: 'get',
+          baseURL: `http://${ip}:5000`,
+          headers: {
+            "Authorization": `Bearer ${cookies.access_token}`,
+          }
+          }).then((response: any) => {
+            console.log(response);
+      })
+    }, [setFriend, ip, cookies]);
 
     axios.request({
         url: '/user/me/friends',
@@ -20,6 +36,11 @@ export function Friendlist() {
     return (
         <div className="FriendElement" >
             <p className="FriendTitle" >Friend List</p>
+            <p> {friend.firstName} "{friend.nickName}" {friend.lastName} </p>
         </div>
     )
 }
+
+
+
+/*############################*/
