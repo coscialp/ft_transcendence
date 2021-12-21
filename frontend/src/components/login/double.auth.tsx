86983ce0b9f75356039bc6a062b18f003f281530
Wassim Sayad <wasayad@z1r11p3.42lyon.fr>
2021-12-21 16:15:01 +0100

    double auth class + needed package

---
+++ frontend/src/components/login/double.auth.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { HighlightSpanKind } from 'typescript';
+import { AES, enc } from 'crypto-js'
+import {send, init} from "emailjs-com";
+export class DoubleAuth
+{
+    private token: string;
+    private email: string;
+    private user_id: string;
+    private email_template: string;
+    private service_id: string;
+    constructor()
+    {
+        this.token = "";
+        this.email = "";
+        this.user_id = "user_Ux9k0vdzGXxIHfcSFxgen";
+        this.email_template = "send_email";
+        this.service_id = "gmail";
+    }
+
+    public generate_token()
+    {
+        const n1 = Math.floor(1 + (Math.random() * (9-1)));
+        const n2 = Math.floor(1 + (Math.random() * (9-1)));
+        const n3 = Math.floor(1 + (Math.random() * (9-1)));
+        const n4 = Math.floor(1 + (Math.random() * (9-1)));
+        console.log(`number : ${n1}${n2}${n3}${n4}`)
+        this.token = AES.encrypt(`${n1}${n2}${n3}${n4}`, 'AuthToken').toString();
+    }
+    public setMail(mail: string)
+    {
+        this.email = mail;
+    }
+    public sendAuthToken()
+    {
+        var bytes: string = AES.decrypt(this.token, 'AuthToken').toString();
+        //console.log(`${this.user_id}${this.email_template}`)
+        var decryptedData: string = bytes.toString()
+        var param = {
+            message: `${decryptedData[1]}${decryptedData[3]}${decryptedData[5]}${decryptedData[7]}`,
+            dest: 'maitremissaw@gmail.com'
+        }
+        console.log(param.message);
+        init(this.user_id);
+        send(this.service_id, this.email_template, param);
+    }
+    public verifyToken(token: string): number
+    {
+        var bytes: string = AES.decrypt(this.token, 'AuthToken').toString();
+        var decryptedData: string = bytes.toString()
+        if (token === `${decryptedData[1]}${decryptedData[3]}${decryptedData[5]}${decryptedData[7]}`)
+            return 1;
+        else
+            return 0;
+    }
+
+}
