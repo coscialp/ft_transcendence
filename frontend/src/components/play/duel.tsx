8439b4471e2813d5f06707c00e815dbd6c28f826
akerdeka <kerdea@yahoo.fr>
2022-01-20 12:28:21 +0100

    push

--- frontend/src/components/play/duel.tsx
+++ frontend/src/components/play/duel.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useHistory } from 'react-router';
 import { useCookies } from "react-cookie";
 import "./duel.css";
